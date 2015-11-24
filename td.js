098443563f0f242667d1a57f71d1d75c3a2716a8
Wukai1220 <1183323265@qq.com>
2015-11-24 20:40:03 +0800

    first commit

---
+++ td.js
@@ -0,0 +1,87 @@
+/**
+ * Created by Wukai on 2015/11/24.
+ */
+
+
+function init() {
+
+}
+
+function snowParticle(dir, x, y) {
+
+}
+
+function wallTower(x, y) {
+
+}
+
+function slowTower(x, y) {
+
+}
+
+function aoeTower(x, y) {
+
+}
+
+function laserTower(x, y) {
+
+}
+
+function monster(level) {
+
+}
+
+function draw() {
+
+}
+
+function mouseDown(e) {
+
+}
+
+function sell() {
+
+}
+
+function mouseMove(e) {
+
+}
+
+function upgrade() {
+
+}
+
+function controlsUpdate() {
+
+}
+
+function restart() {
+
+}
+
+function numberFormat(val) {
+
+}
+
+function Point(x, y) {
+
+}
+
+function VisPoint(x, y) {
+
+}
+
+var directions;
+
+function calculatePath(startPoint, endPoint, obstacles) {
+
+}
+
+var path;
+function genPath() {
+
+}
+
+document.onkeydown = function(e) {
+
+};
