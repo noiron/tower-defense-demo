893d8b5c523eb3b9da99704c16b40eff550aeaa8
Wukai1220 <1183323265@qq.com>
2015-11-24 23:54:23 +0800

    2015-11-24 commit

--- td.js
+++ td.js
@@ -3,8 +3,46 @@
  */
 
 
-function init() {
+var canvas = document.getElementById("drawing");
+var context = canvas.getContext("2d");
 
+
+var gridWidth, gridHeight;
+var towers = [];
+var monsters = [];
+var size = 10;      // how many columns and rows
+var towerCosts = [40, 200, 1000, 10];
+
+var playerHealth = 10;
+
+init();
+requestAnimFrame = (function() {
+    return window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function(callback) {
+                window.setTimeout(callback, 1000/30);
+            };
+    })();
+
+requestAnimFrame(draw);
+
+
+function init() {
+    // calculate grid size by canvas's size and cols/rows num
+    gridWidth = Math.floor((canvas.width -size) / size);
+    gridHeight = Math.floor((canvas.width -size) / size);
+
+    controlsUpdate();
+
+    document.getElementById("cTower1Bt").value = "Place laser tower(" + numberFormat(towerCosts[0]) + ")";
+    document.getElementById("cTower2Bt").value = "Place AOE tower(" + numberFormat(towerCosts[1]) + ")";
+    document.getElementById("cTower3Bt").value = "Place slow tower(" + numberFormat(towerCosts[2]) + ")";
+    document.getElementById("cTower4Bt").value = "Place wall(" + numberFormat(towerCosts[3]) + ")";
+
+    generatePath();
 }
 
 function snowParticle(dir, x, y) {
@@ -32,8 +70,33 @@ function monster(level) {
 }
 
 function draw() {
+    requestAnimFrame(draw);
+    if (playerHealth <= 0) return;
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
+    context.globalAlpha = 1;
+    context.fillStyle = "#aaaaaa";
+
+    context.strokeStyle = "#000";
+    context.lineWidth = 1;
+    context.beginPath();
+
+    // Draw vertical lines
+    for (var i = 0; i < 21; i++) {
+        context.moveTo(i * gridWidth + i, 0);
+        context.lineTo(i * gridWidth + i, canvas.height);
+    }
+    context.stroke();
+
+    // Draw horizontal lines
+    for (i = 0; i < 21; i++) {
+        context.moveTo(0, i * gridWidth + i);
+        context.lineTo(canvas.width, i * gridWidth + i);
+    }
+    context.stroke();
 }
+
+//draw();
 
 function mouseDown(e) {
 
@@ -44,7 +107,7 @@ function sell() {
 }
 
 function mouseMove(e) {
-
+    var mouseX, mouseY;
 }
 
 function upgrade() {
@@ -59,8 +122,15 @@ function restart() {
 
 }
 
-function numberFormat(val) {
 
+/** Format the number, add ',' every 3 digits **/
+function numberFormat(val) {
+    string = '' + val;
+
+    var regex = /^([0-9]+)([0-9]{3})/;
+    while (regex.test(string)) {
+        string = string.replace(regex, '$1, $2');
+    }
 }
 
 function Point(x, y) {
@@ -78,7 +148,7 @@ function calculatePath(startPoint, endPoint, obstacles) {
 }
 
 var path;
-function genPath() {
+function generatePath() {
 
 }
 
